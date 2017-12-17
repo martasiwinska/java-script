5f07d4a56968c84297ff83430c5849165cc25068
Marta Siwińska <mar.siwinska@gmail.com>
2017-12-17 16:58:02 +0100

    json

---
+++ js/12-json.js
@@ -0,0 +1,66 @@
+'use strict'
+
+var jsonOsoby = {
+    "osoby": [
+        {
+            imie: "Krystian",
+            nazwisko: "Dziopa",
+            wzrost: 180,
+            oczy: "niebieskie",
+            zainteresowania: [
+                {
+                    nazwa: "podroze"
+                },
+                {
+                    nazwa: "gotowanie"
+                }
+            ]
+        },
+        {
+            imie: "Dominik",
+            nazwisko: "Gomuła",
+            wzrost: 170,
+            oczy: "brązowe",
+            zainteresowania: [
+                {
+                    nazwa: "gotowanie"
+                },
+                {
+                    nazwa: "języki"
+                }
+            ]
+        },
+        {
+            imie: "Claudia",
+            nazwisko: "Żelazowska",
+            wzrost: 167,
+            oczy: "brązowe",
+            zainteresowania: [
+                {
+                    nazwa: "programowanie"
+                },
+                {
+                    nazwa: "podroze"
+                },
+                {
+                    nazwa: "języki"
+                }
+            ]
+
+        }
+    ]
+}
+
+console.log(jsonOsoby.osoby[1].zainteresowania);
+
+jsonOsoby.osoby.forEach(function (e) {
+    console.log(e);
+});
+
+jsonOsoby.osoby.forEach(function (e) {
+    console.log(e.wzrost);
+});
+
+jsonOsoby.osoby.forEach(function (e) {
+    console.log(e.zainteresowania);
+});
