fa95e416b1fe2d40be31b53d523982c7d722c2c4
Marta Siwińska <mar.siwinska@gmail.com>
2017-12-17 12:14:42 +0100

    typy danych

---
+++ js/5-datatypes.js
@@ -0,0 +1,34 @@
+'use strict';
+
+//typ liczbowy
+
+var wyplata = 5000;
+var premia = 2500;
+var wyplataCalkowita;
+
+wyplataCalkowita = wyplata + premia;
+console.log(wyplataCalkowita);
+
+//typ łańcuchowy
+
+var wyplataStr = "5000";
+var premiaStr = "2500";
+//cudzusłów w cudzysłowie zamknie wartosc i po nim wywali błąd możemy zamiast tego dać znakk specjalny /
+var wyplataCalkowitaStr;
+
+wyplataCalkowitaStr = wyplataStr + premiaStr;
+console.log(wyplataCalkowitaStr);
+
+//typ logiczny boolean 
+var czyJestSmog;
+czyJestSmog = true;
+console.log(czyJestSmog);
+
+//typy specjalne -- undefined null
+
+var niezdefiniowanaZmienna;
+var nullowaZmienna = null;
+
+console.log(niezdefiniowanaZmienna);
+console.log(nullowaZmienna);
+
