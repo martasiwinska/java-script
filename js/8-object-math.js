dd5986c87fc5654da6673a947ba4262f3864ccb6
Marta Siwińska <mar.siwinska@gmail.com>
2017-12-17 14:55:38 +0100

    operatory

--- js/8-object-math.js
+++ js/8-object-math.js
@@ -1,5 +1,7 @@
 'use strict';
 
+//Object Math
+
 console.log( Math.PI);
 
 console.log( Math.cos(0));
---
+++ js/9-operator.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var liczba1 = 13, liczba2 = 5;
+var wynik;
+wynik = liczba1 % liczba2;
+console.log(wynik);
+
+//liczba1 = liczba1 +liczba2;
+//console.log(liczba1);
+
+//operatory przypisania
+
+liczba1 += liczba2;
+console.log(liczba1);
+
+//operatory porównania
+
+if ("2" === 2) {
+    console.log("prawda");
+} else {
+    console.log("falsz");
+}
+
+
+if ( !( ("2" === 2 ) && (8 , 4) ) ) {
+    document.write("<br>prawda");
+}
